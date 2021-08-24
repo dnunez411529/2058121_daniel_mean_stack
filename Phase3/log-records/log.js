431b65dae35883ea19e67550820a02f9abb0751a
dnunez411529 <64983598+dnunez411529@users.noreply.github.com>
2021-08-23 21:05:08 -0700

    completed log node project

---
+++ Phase3/log-records/log.js
@@ -0,0 +1,37 @@
+let fs = require('fs');
+
+// Used readline-sync to take input from user
+let readline = require('readline-sync');
+let fname = readline.question('Enter first name: ');
+let lname = readline.question('Enter last name: ');
+let gender = readline.question('Enter gender: ');
+let email = readline.questionEMail('Enter email: ');
+let date = new Date();
+
+// Create a object with the users information and also the current date and time
+let msg = {
+  fname: fname,
+  lname: lname,
+  gender: gender,
+  email: email,
+  date: date.toDateString(),
+  time: date.toLocaleDateString(),
+};
+
+debugger;
+
+// Read the current userInfo.json file, if it doesn't exist the 'a+' flag creates one, and then stored the data in userData.
+let userData = fs.readFileSync('userInfo.json', { flag: 'a+' }).toString();
+let userJson = [];
+// If the current json file isn't empty, parse it and append current info
+if (userData.length > 0) {
+  userJson = JSON.parse(userData);
+  userJson.push(msg);
+} else {
+  // If empty just push the current user info
+  userJson.push(msg);
+}
+
+// Replace previous json info with updated one
+fs.writeFileSync('userInfo.json', JSON.stringify(userJson));
+console.log('New user entry created!');
